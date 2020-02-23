6ff834d2ccf6947165bd77cd372a0f37934ba3ae
birsen-sinem <bsgebes@gmail.com>
2020-02-23 16:14:52 -0600

    First Commit

---
+++ src/day37/MethodOverloadongWithWrapper.java
@@ -0,0 +1,13 @@
+package day37;
+
+public class MethodOverloadongWithWrapper {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void printnum (int x){
+
+    }
+}
---
+++ src/day41/Product.java
@@ -0,0 +1,28 @@
+package day41;
+
+// Make this Product a well encapsulated
+// make all the instance fields private so it can only be accessible in same class
+// provide public getters and setters method
+
+public class Product {
+
+   private String name ;
+   private double price;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
